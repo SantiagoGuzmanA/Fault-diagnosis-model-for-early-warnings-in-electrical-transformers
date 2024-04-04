ffb5d7116b857e85289ce3c8fd63a1195eb32281
santiagoguzman251060 <santiago.a.0913@gmail.com>
2024-04-03 23:19:29 -0500

    Grafica de datos

--- src/importDB.py
+++ src/importDB.py
@@ -10,6 +10,7 @@ def importDB(DataBase):
         row = [float(value) if value.replace('.', '', 1).isdigit() else value for value in line.strip().split(',')]
         DB.append(row)
     return(DB)
+
 def deleteZeros(DataBase,Column):
     try:
         if not isinstance(DataBase,list):
@@ -23,6 +24,32 @@ def deleteZeros(DataBase,Column):
             DB.append(row)
     return(DB)
 
+    
+import matplotlib.pyplot as plt
+
+def plot_column(column_data):
+    plt.plot(column_data)
+    plt.xlabel('Índice de fila')
+    plt.ylabel('Valor')
+    plt.title('Gráfico de la columna filtrada')
+    plt.grid(True)
+    plt.show()
+
+def convert_column_to_float(DB, column_index):
+    column_data = [row[column_index] for row in DB]
+    converted_column = []
+
+    for value in column_data:
+        try:
+            converted_value = float(value)
+            converted_column.append(converted_value)
+        except ValueError:
+        
+            pass
+
+    return converted_column
+
+    
 #fname = input("Ingrese el nombre del archivo: ")
 #DB = importDB(fname)
 #for i in DB:
--- src/main.py
+++ src/main.py
@@ -12,13 +12,21 @@
 #       -....
 from importDB import*
 
-fname = input("Ingrese el nombre del archivo: ")
+fname = "DatosMonitoringTransformer.txt"
 column = input("Ingrese la columna de la potencia: ")
+
 DB = importDB(fname)
 print("Base de datos con ceros")
 for i in DB:
     print(i)
 print("Base de datos sin ceros")
+
 DB = deleteZeros(DB,int(column))
 for i in DB:
     print(i)
+
+column_index = 1
+
+column_data_float = convert_column_to_float(DB, column_index)
+
+plot_column(column_data_float) 
