f0e5fb67366fe46af1181779b306941f34c9b2a8
santiagoguzman251060 <santiago.a.0913@gmail.com>
2024-04-18 19:38:14 -0500

    Cambio en import data base

--- src/importDB.py
+++ src/importDB.py
@@ -1,12 +1,12 @@
 def impDB(DataBase):
     try:
-        fhand = open(DataBase)
+        Data = open(DataBase)
     except:
         print("El archivo .txt no existe en la ruta especificada")
         quit()
     
     DB = []
-    for line in fhand:
+    for line in Data:
         row = [float(value) if value.replace('.', '', 1).isdigit() else value for value in line.strip().split(',')]
         DB.append(row)
     return(DB)
