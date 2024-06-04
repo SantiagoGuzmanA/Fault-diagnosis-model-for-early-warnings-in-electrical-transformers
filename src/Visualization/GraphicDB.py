5016bf2e8fd043b6b7613f2948d60efcbe8eeabd
santiagoguzman251060 <santiago.a.0913@gmail.com>
2024-06-04 08:36:13 -0500

    Carpeta con graficos como imagenes

--- src/Visualization/GraphicDB.py
+++ src/Visualization/GraphicDB.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
-from PowerCalculation import*
+import sys
+import os
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+sys.path.append(project_root)
+from src.Analysis.PowerCalculation import*
 def plot_column(DataBase,column_G):
     column_data = [row[column_G] for row in DataBase[1:]]
 
@@ -9,7 +13,9 @@ def plot_column(DataBase,column_G):
     plt.ylabel('Current 1')
     plt.title('Line 1 Current Graph')
     plt.grid(True)
+    plt.savefig('results//figures//Line 1 Current Graph.png',bbox_inches='tight',pad_inches=0.05)
     plt.show()
+    plt.close()
 
 def plot_column1(DataBase,column_G1):
     column_data1 = [row[column_G1] for row in DataBase[1:]]
@@ -33,30 +39,13 @@ def plot_column2(DataBase, column_G2):
     plt.grid(True)
     plt.show()
 
-def plot_power(DataBase):
-    powers = calculate_power_per_recordOP(DataBase)
-
-    plt.figure(figsize=(12, 6))
-
-    plt.plot(powers, color='r')
-
-    plt.xlabel('# Of data')
-    plt.ylabel('Power (KvA)')
-    plt.title('Power graph per record')
-
-    plt.grid(True)
-
-    plt.show()
-
 def plot_power_bar(DataBase):
     powers = calculate_power_per_recordOP(DataBase)
     indices = range(1, len(powers) + 1)
-
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.bar(indices, powers, color='red', alpha=0.7)
-
     ax.set_xlabel('# Of data')
     ax.set_ylabel('Power (KvA)')
     ax.set_title('Power graph per record (Bars)')
-
-    plt.show()
+    plt.savefig('results//figures//Power graph per record (Bars).png',bbox_inches='tight',pad_inches=0.05)
+    #plt.show()
