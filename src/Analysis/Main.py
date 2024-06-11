ab3e278dd757f539cfccefcec2b87edaf9246f7b
santiagoguzman251060 <santiago.a.0913@gmail.com>
2024-06-11 07:31:42 -0500

    Docstrings in main

--- src/Analysis/Main.py
+++ src/Analysis/Main.py
@@ -1,29 +1,10 @@
-# Este es el programa principal que va a llamar las funciones
-#Main : Este programa extrae la informacion de una base de datos guardada en un archivo .txt, para posteriormente hacerle el siguiente tratamiento:
-#   - Guardar la base de datos en un elemento tipo lista
-#   - Eliminar las filas que contengan un valor 0 en la columna especificada
-#   - Graficar la columna del voltaje en funcion del tiempo 
-#   - Contar las filas que se eliminaron por tener un 0 
-#   - Mostrar las filas que quedaron despues de las eliminadas por 0
-#   - Hacer el calculo de la potencia y verificarlo con la columna de la potencia para garantizar que esten bien los datos
-#   - ....
-# Los readme se deben de hacer para cada carpeta
-#   - ....
-#   INPUT:
-#       - Nombre del archivo de la base de datos con su respectiva extención: Solo compatible con archivos .txt
-#       - Numero de la columna en la que se desea buscar valores 0: Se empieza a contar a partir de 1
-#       - ....
-#   OUTPUT
-#       - Numero de filas en 0 
-#       - Numero de filas restantes 
-#       - Grafica del voltaje
-#       - Visualizacion de las filas corregidas por potencia
-#       - ....
 import sys
 import os
+# Add project root to the system path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 sys.path.append(project_root)
 print(project_root)
+# Import modules from project
 from src.Preprocessing.ImportDB import *
 from src.Preprocessing.DeleteZerosDB import*
 from src.Visualization.GraphicDB import*
@@ -33,23 +14,27 @@ from src.Analysis.AtypicalValues import*
 
 fname = "DatosMonitoringTransformer.txt"
 column = [4, 5, 6]
-#importar base de datos
+# Import database
 DB = impDB(fname)
 print("Tamaño de la Base de datos con ceros: " + str(len(DB)))
-#Deletezeros
+
+# Delete rows with zeros
 DBZ = deleteZerosOP(DB,column)
 print("Tamaño de la Base de datos sin ceros: " + str(len(DBZ)))
-#Graphics
+
+# Generate graphs for specified columns
 plot_column(DBZ,4)
 plot_column1(DBZ,5)
 plot_column2(DBZ,6)
 plot_power_bar(DBZ)
-#power calculation
+
+# Calculate power per record
 potencias_por_registro = calculate_power_per_recordOP(DBZ)
 print("Valores de las primeras 30 filas de la base de datos y potencias calculadas:")
 for i, (registro, potencia) in enumerate(zip(DBZ[1:31], potencias_por_registro[:30]), 1):
     print(f'Registro {i}: Datos = {registro}, Potencia = {potencia} KvA')
-#standard deviation
+    
+# Calculate standard deviation
 DE1,DE2,DE3,LIM1I1,LIM2I1,LIM1I2,LIM2I2,LIM1I3,LIM2I3 = standard_deviationOP(DBZ)
 print(f"Standard deviation I1: {DE1}")
 print(f"Standard deviation I2: {DE2}")
@@ -60,7 +45,8 @@ print(f"Upper limit I2: {LIM1I2}")
 print(f"Lower limit I2: {LIM2I2}")
 print(f"Upper limit I3: {LIM1I3}")
 print(f"Lower limit I3: {LIM2I3}\n")
-#Atypical values
+
+# Filter outlier values
 valores_dentro_limites, valores_atipicos = filter_outlier_values(DBZ, LIM1I1, LIM2I1, LIM1I2, LIM2I2, LIM1I3, LIM2I3)
 print("Valores dentro de los límites:")
 print(len(valores_dentro_limites))
--- src/Analysis/MainPandas.py
+++ src/Analysis/MainPandas.py
@@ -1,50 +1,67 @@
 import sys
 import os
+
+# Adding project root to the system path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 sys.path.append(project_root)
+
+# Importing functions from custom modules
 from src.Preprocessing.WorkPandas import*
 from src.Analysis.AnalysisPandas import*
 from src.Visualization.VisualizationPandas import*
+
+# File path
 file_path = os.path.join(project_root, 'data', 'raw', 'DatosMonitoringTransformer.txt')
+
+# Processing data
 df, dfRename, dfZeros = process_transformer_data(file_path)
+
+# Grouping features
 groups = {
         "Phase Voltages": ["Phase 1 Voltage", "Phase 2 Voltage", "Phase 3 Voltage"],
         "Phase Currents": ["Phase 1 Current", "Phase 2 Current", "Phase 3 Current"],
         "Voltage Differences": ["1-2 Voltage", "2-3 Voltage", "3-1 Voltage"]
     }
-#import Dataframe 
+
+# Importing DataFrame
 print("Here we can see a summary of the information in the dataframe: \n")
 df.info()
-#Rename columns
+
+# Renaming columns
 print("Here we can see a summary of the dataframe information but now with the modified column names: \n")
 dfRename.info()
 print("\n")
 print("Viewing the first rows of the DataFrame as a table: \n")
 print(dfRename.head(10))
-#Delete zeros
+
+# Deleting rows with zeros
 print("Here we can see a summary of the information in the dataframe after removing the rows with zeros: \n")
 dfZeros.info()
 print("\n")
 print("Viewing the first rows of the DataFrame with zeros as a table: \n")
 print(dfZeros.head(10))
-#statistics
+
+# Summary statistics
 print("Summary of statistics:")
 print("\n")
 print(dfZeros.describe())
-#Shapiro-Wilk Test for Phase Currents:
+
+# Shapiro-Wilk Test for Phase Currents
 shapiro_results = shapiro_tests(dfZeros, groups)
 for group, results in shapiro_results.items():
         print(f"Shapiro-Wilk Test for {group}:")
         print("\n")
         for column, result in results.items():
             print(f"{column}: W-statistic = {result.statistic}, p-value = {result.pvalue}")
-#Kolmogorov-Smirnov Test for Phase Voltages:
+            
+# Kolmogorov-Smirnov Test for Phase Voltages
 ks_results = kstest_multiple_groups(dfZeros, groups, 'norm')
 for group, results in ks_results.items():
         print(f"Kolmogorov-Smirnov Test for {group}:")
         for column, result in results.items():
             print(f"{column}: KS-statistic = {result.statistic}, p-value = {result.pvalue}")
-#Kruskal-Wallis Test:    
+            
+# Kruskal-Wallis Test   
 group_name = "Phase Currents"      
 h_statistic, p_value = kruskal_wallis_test(dfZeros, groups, group_name)
 print("Kruskal-Wallis Test:")
@@ -55,26 +72,30 @@ if p_value < alpha:
     print("Reject the null hypothesis. There is a significant difference between the groups.")
 else:
     print("Fail to reject the null hypothesis. There is no significant difference between the groups.")
-#Quantitative separability
-#fisher
+    
+# Quantitative separability (Fisher Discriminant Ratio)
 fisher_ratios = calculate_fisher_discriminant_ratio(dfZeros)
 print('\nFisher Discriminant Ratio:\n')
 for feature, ratio in fisher_ratios.items():
     print(f"{feature}: {ratio}")
-#AUC
+    
+# AUC scores
 for group_name, group_features in groups.items():
         print(f"AUC scores for {group_name} features:")
         auc_scores = calculate_auc_scores(dfZeros, group_features, group_name)
         for feature_name, auc_score in zip(group_features, auc_scores):
             print(f"AUC for {feature_name}: {auc_score}")
-#correlation
+            
+# Correlation with target variables
 target_variables = ["Phase 1 Current", "Phase 2 Current", "Phase 3 Current"]
 correlation_results = calculate_correlation_with_target(dfZeros, target_variables)
 for target_variable, correlation_with_target in correlation_results.items():
     print("Correlation with target variable '{}':".format(target_variable))
     print(correlation_with_target)
     print()
-#Random Forest Classifier
+    
+# Random Forest Classifier
 random_forest_classifier(dfRename)
-#Visualization
+
+# Visualization
 plot_histograms(dfZeros)
