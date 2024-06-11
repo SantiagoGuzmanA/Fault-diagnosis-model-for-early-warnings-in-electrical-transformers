57938b64d185d63046f12ff75b700cf716a7f6c8
santiagoguzman251060 <santiago.a.0913@gmail.com>
2024-06-10 19:04:17 -0500

    Prueba repository

--- src/Analysis/MainPandas.py
+++ src/Analysis/MainPandas.py
@@ -1,3 +1,18 @@
+# Este es el programa principal que va a llamar las funciones
+#Main : Este programa extrae la informacion de una base de datos guardada en un archivo .txt, para posteriormente hacerle el siguiente tratamiento:
+#   - Guardar la base de datos en un elemento tipo lista
+#   - Eliminar las filas que contengan un valor 0 en la columna especificada
+#   - Graficar la columna del voltaje en funcion del tiempo 
+#   - Contar las filas que se eliminaron por tener un 0 
+#   - Mostrar las filas que quedaron despues de las eliminadas por 0
+#   - Hacer el calculo de la potencia y verificarlo con la columna de la potencia para garantizar que esten bien los datos
+#   - ....
+# Los readme se deben de hacer para cada carpeta
+#   - ....
+#   INPUT:
+#       - Nombre del archivo de la base de datos con su respectiva extención: Solo compatible con archivos .txt
+#       - Numero de la columna en la que se desea buscar valores 0: Se empieza a contar a partir de 1
+#       - ....
 import sys
 import os
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
